a7a367da25f59d1446edd15047ee3f0ae7f83d5f
aubreyghoward <aubrey.g.howard@rice.edu>
2017-09-11 23:03:57 -0500

    Q3PartialCommit

--- HW1.m
+++ HW1.m
@@ -253,6 +253,7 @@ hold on
 end
 
 disp ('Terminated Analysis') %Aids in reading Command window runs.
+
 %%
 %part 5: Make sure your results from part 4 are sensible. What features
 % must this curve have (hint: what should be the value when N is small or when
@@ -272,6 +273,29 @@ disp ('Terminated Analysis') %Aids in reading Command window runs.
 
 % part1: write code to read the Cp data from this file into a vector. You can ignore the last two
 % rows with positions beginning with G and H as there were no samples here. 
+clear all
+
+fid = fopen('qPCRdata.txt', 'r');
+line1 = fgetl(fid);
+line1 = fgetl(fid);
+
+q = 1;
+str_mat = [1,1];
+
+while line1 ~= -1
+    var1 = strsplit(line1," ");
+    lengthofmat = [1,length(var1)];
+    %if lengthofmat == str_mat(q,:)
+      %  str_mat(q,:)= var1
+      %%  line1 = fgetl(fid);
+        %q = q+1
+  %  else
+     %   break
+        
+   % end
+end
+
+
 
 % Part 2: transform this vector into an array representing the layout of
 % the plate. e.g. a 6 row, 12 column array should that data(1,1) = Cp from
@@ -308,75 +332,3 @@ disp ('Terminated Analysis') %Aids in reading Command window runs.
 % uncertainty to the final results. Add error bars to your plot. (on
 % propagation of error, see, for example:
 % https://en.wikipedia.org/wiki/Propagation_of_uncertainty
-
-
-
-%part 2: open reading frames (ORFs) are pieces of DNA that can be
-% transcribed and translated. They start with a start codon (ATG) and end with a
-% stop codon (TAA, TGA, or TAG). Write a piece of code that finds the longest ORF 
-% in your seqeunce rand_seq. Hint: see the function strfind.
-
-%part 3: copy your code in parts 1 and 2 but place it inside a loop that
-% runs 1000 times. Use this to determine the probability
-% that an sequence of length 500 has an ORF of greater than 50 b.p.
-
-%part 4: copy your code from part 3 but put it inside yet another loop,
-% this time over the sequence length N. Plot the probability of having an
-% ORF > 50 b.p. as a funciton of the sequence length. 
-
-%part 5: Make sure your results from part 4 are sensible. What features
-% must this curve have (hint: what should be the value when N is small or when
-% N is very large? how should the curve change in between?) Make sure your
-% plot looks like this. 
-
-%% problem 3 data input/output and simple analysis
-
-%The file qPCRdata.txt is an actual file that comes from a Roche
-%LightCycler qPCR machine. The important columns are the Cp which tells
-%you the cycle of amplification and the position which tells you the well
-%from the 96 well plate. Each column of the plate has a different gene and
-%each row has a different condition. Each gene in done in triplicates so
-%columns 1-3 are the same gene, columns 4-6 the same, etc.
-%so A1-A3 are gene 1 condition 1, B1-B3 gene 1 condition 2, A4-A6 gene 2
-%condition 1, B4-B6 gene2 condition 2 etc. 
-
-% part1: write code to read the Cp data from this file into a vector. You can ignore the last two
-% rows with positions beginning with G and H as there were no samples here. 
-
-% Part 2: transform this vector into an array representing the layout of
-% the plate. e.g. a 6 row, 12 column array should that data(1,1) = Cp from
-% A1, data(1,2) = Cp from A2, data(2,1) = Cp from B1 etc. 
-
-% Part 3. The 4th gene in columns 10 - 12 is known as a normalization gene.
-% That is, it's should not change between conditions and it is used to normalize 
-% the expression values for the others. For the other three
-% genes, compute their normalized expression in all  conditions, normalized to condition 1. 
-% In other words, the fold change between these conditions and condition 1. The
-% formula for this is 2^[Cp0 - CpX - (CpN0 - CpNX)] where Cp0 is the Cp for
-% the gene in the 1st condition, CpX is the value of Cp in condition X and
-% CpN0 and CpNX are the same quantitites for the normalization gene.
-% Plot this data in an appropriate way. 
-
-
-%% Challenge problems that extend the above (optional)
-
-% 1. Write a solution to Problem 2 part 2 that doesn't use any loops at
-% all. Hint: start by using the built in function bsxfun to make a matrix of all distances
-% between start and stop codons. 
-
-% 2. Problem 2, part 4. Use Matlab to compute the exact solution to this
-% problem and compare your answer to what you got previously by testing
-% many sequences. Plot both on the same set of axes. Hint: to get started 
-% think about the following:
-% A. How many sequences of length N are there?
-% B. How many ways of making an ORF of length N_ORF are there?
-% C. For each N_ORF how many ways of position this reading frame in a
-% sequence of length N are there?
-
-% 3. Problem 3. Assume that the error in each Cp is the standard deviation
-% of the three measurements. Add a section to your code that propogates this
-% uncertainty to the final results. Add error bars to your plot. (on
-% propagation of error, see, for example:
-% https://en.wikipedia.org/wiki/Propagation_of_uncertainty
-
-
